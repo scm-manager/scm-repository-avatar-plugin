584bb21e022e7cdbb1f2f8f1913591e0cee996ff
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2022-01-24 14:31:48 +0100

    Improve modal accessibility
    Use core changes to improve modal accessibility.
    
    Co-authored-by: Matthias Thieroff <matthias.thieroff@cloudogu.com>

--- src/main/js/predefined/PredefinedAvatarSelector.tsx
+++ src/main/js/predefined/PredefinedAvatarSelector.tsx
@@ -22,7 +22,7 @@
  * SOFTWARE.
  */
 
-import React, { FC, useState } from "react";
+import React, { FC, useRef, useState } from "react";
 import { Button, FullscreenModal } from "@scm-manager/ui-components";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
@@ -37,11 +37,13 @@ const Selector = styled.div`
   margin-bottom: 0.46rem;
 `;
 
-const Chooser = styled.div`
+const Chooser = styled.button`
   height: 64px;
   width: 64px;
   margin-right: 1rem;
   margin-bottom: 0.75rem;
+  padding: 0;
+  border-radius: 5px;
 `;
 
 const Palette = styled.div`
@@ -61,6 +63,7 @@ type Props = {
 const PredefinedAvatarSelector: FC<Props> = ({ icon, color, setIcon, setColor, disabled }) => {
   const [t] = useTranslation("plugins");
   const [avatarModal, setAvatarModal] = useState(false);
+  const initialFocusRef = useRef<HTMLButtonElement>(null);
 
   const renderIconPalette = (label: string, icons: string[]) => {
     return (
@@ -68,44 +71,23 @@ const PredefinedAvatarSelector: FC<Props> = ({ icon, color, setIcon, setColor, d
         <StyledLabel>{label}</StyledLabel>
         <Palette>
           {icons.map((iconName, index) => (
-            <Chooser onClick={() => setIcon(iconName)} color="white" key={index}>
+            <Chooser
+              onClick={() => setIcon(iconName)}
+              color="white"
+              key={index}
+              className="button"
+              onKeyPress={(event: React.KeyboardEvent<HTMLButtonElement>) => {
+                if (event.key === "Enter") {
+                  setIcon(iconName);
+                  setAvatarModal(false);
+                }
+              }}
+            >
               <PredefinedAvatar avatar={{ type: "PREDEFINED", iconName, color, _links: {} }} />
             </Chooser>
           ))}
         </Palette>
       </>
-    );
-  };
-
-  const renderAvatarModal = () => {
-    return (
-      <FullscreenModal
-        body={
-          <>
-            <AvatarPreview
-              avatar={<PredefinedAvatar avatar={{ iconName: icon, color: color, type: "PREDEFINED", _links: {} }} />}
-              className="mr-4"
-            />
-            <hr />
-            <StyledLabel>{t("scm-repository-avatar-plugin.avatarSelector.colors")}</StyledLabel>
-            <Palette>
-              {colors.map(c => (
-                <Chooser onClick={() => setColor(c)}>
-                  <PredefinedAvatar avatar={{ iconName: icon, color: c, type: "PREDEFINED", _links: {} }} />
-                </Chooser>
-              ))}
-            </Palette>
-            <hr />
-            {renderIconPalette(t("scm-repository-avatar-plugin.avatarSelector.icons.technology"), technologyIcons)}
-            {renderIconPalette(t("scm-repository-avatar-plugin.avatarSelector.icons.business"), businessIcons)}
-            {renderIconPalette(t("scm-repository-avatar-plugin.avatarSelector.icons.other"), otherIcons)}
-          </>
-        }
-        active={avatarModal}
-        title={t("scm-repository-avatar-plugin.avatarSelector.avatarModal")}
-        closeFunction={() => setAvatarModal(false)}
-        closeButtonLabel={t("scm-repository-avatar-plugin.avatarSelector.selectButton")}
-      />
     );
   };
 
@@ -120,7 +102,44 @@ const PredefinedAvatarSelector: FC<Props> = ({ icon, color, setIcon, setColor, d
         label={t("scm-repository-avatar-plugin.avatarSelector.avatarModal")}
         disabled={disabled}
       />
-      {renderAvatarModal()}
+      <FullscreenModal
+        body={
+          <>
+            <AvatarPreview
+              avatar={<PredefinedAvatar avatar={{ iconName: icon, color: color, type: "PREDEFINED", _links: {} }} />}
+              className="mr-4"
+            />
+            <hr />
+            <StyledLabel>{t("scm-repository-avatar-plugin.avatarSelector.colors")}</StyledLabel>
+            <Palette>
+              {colors.map((c, index) => (
+                <Chooser
+                  className="button"
+                  onClick={() => setColor(c)}
+                  ref={index === 0 ? initialFocusRef : undefined}
+                  onKeyPress={(event: React.KeyboardEvent<HTMLButtonElement>) => {
+                    if (event.key === "Enter") {
+                      setColor(c);
+                      setAvatarModal(false);
+                    }
+                  }}
+                >
+                  <PredefinedAvatar avatar={{ iconName: icon, color: c, type: "PREDEFINED", _links: {} }} />
+                </Chooser>
+              ))}
+            </Palette>
+            <hr />
+            {renderIconPalette(t("scm-repository-avatar-plugin.avatarSelector.icons.technology"), technologyIcons)}
+            {renderIconPalette(t("scm-repository-avatar-plugin.avatarSelector.icons.business"), businessIcons)}
+            {renderIconPalette(t("scm-repository-avatar-plugin.avatarSelector.icons.other"), otherIcons)}
+          </>
+        }
+        active={avatarModal}
+        title={t("scm-repository-avatar-plugin.avatarSelector.avatarModal")}
+        closeFunction={() => setAvatarModal(false)}
+        closeButtonLabel={t("scm-repository-avatar-plugin.avatarSelector.selectButton")}
+        initialFocusRef={initialFocusRef}
+      />
     </Selector>
   );
 };
